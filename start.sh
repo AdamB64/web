f2736ee98a9d4bf54212313c0ed20512e59578e4
Karma63 <adam2004buchan@outlook.com>
2025-04-02 18:53:15 +0100

    add .env file creation and update start.sh for project setup and dependency installation

--- start.sh
+++ start.sh
@@ -34,18 +34,23 @@ sudo systemctl status mongod
 # Install Git
 sudo apt install -y git
 
+# Clone project
 git clone https://github.com/RobertGordonUniversity/cm4025-coursework-AdamB64.git
 
+# Go into project backend folder
 cd cm4025-coursework-AdamB64/my-app/server
 
+# Create .env file with env variables
 cat <<EOF > .env
 NODE_ENV="development"
 JWT_SECRET="7d0bb34e3b7d4becf8876e377b0b51af0e99faf2188ddf692328140895bd5c9d9905b892c0520c7d5c22745e8d733ac2070dbc74a6d7e976ffd76a8849713df8"
 EOF
 
+# Go back to app root
 cd ..
 
+# Install dependencies
 npm ci
 
+# Run frontend and backend together
 npm run dev:all
-

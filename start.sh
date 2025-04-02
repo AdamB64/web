7d36be21bcaca764d6d7646049f52450e6a7dd72
Karma63 <adam2004buchan@outlook.com>
2025-04-02 18:48:25 +0100

    add .env file creation in start.sh for development environment setup

--- start.sh
+++ start.sh
@@ -36,7 +36,14 @@ sudo apt install -y git
 
 git clone https://github.com/RobertGordonUniversity/cm4025-coursework-AdamB64.git
 
-cd cm4025-coursework-AdamB64/my-app
+cd cm4025-coursework-AdamB64/my-app/server
+
+cat <<EOF > .env
+NODE_ENV="development"
+JWT_SECRET="7d0bb34e3b7d4becf8876e377b0b51af0e99faf2188ddf692328140895bd5c9d9905b892c0520c7d5c22745e8d733ac2070dbc74a6d7e976ffd76a8849713df8"
+EOF
+
+cd ..
 
 npm ci
 

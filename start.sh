974ff24a4f245acfbf3f0db9cef18ec49e6982a5
Karma63 <adam2004buchan@outlook.com>
2025-04-03 15:25:32 +0100

    fix: update start.sh to navigate to the correct project directory and streamline .env file creation

--- start.sh
+++ start.sh
@@ -18,7 +18,7 @@ sudo apt install -y git
 git clone https://github.com/RobertGordonUniversity/cm4025-coursework-AdamB64.git
 
 # Go into project backend folder
-cd cm4025-coursework-AdamB64/my-app/server
+cd cm4025-coursework-AdamB64/my-app
 
 # Create .env file with env variables
 cat <<EOF > .env
@@ -26,8 +26,6 @@ NODE_ENV="development"
 JWT_SECRET="7d0bb34e3b7d4becf8876e377b0b51af0e99faf2188ddf692328140895bd5c9d9905b892c0520c7d5c22745e8d733ac2070dbc74a6d7e976ffd76a8849713df8"
 EOF
 
-# Go back to app root
-cd ..
 
 # Install dependencies
 npm ci
